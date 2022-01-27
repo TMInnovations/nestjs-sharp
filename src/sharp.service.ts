a7f358463c0f435750d92e7f49620778eb073fc8
Thomas Moser <tm@tminnovations.at>
2022-01-27 22:39:30 +0100

    fixed typing problems

--- src/sharp.service.ts
+++ src/sharp.service.ts
@@ -3,5 +3,19 @@ import sharp = require('sharp');
 
 @Injectable()
 export class SharpService {
-  public edit = sharp;
+  public edit: (
+    input?:
+      | Buffer
+      | Uint8Array
+      | Uint8ClampedArray
+      | Int8Array
+      | Uint16Array
+      | Int16Array
+      | Uint32Array
+      | Int32Array
+      | Float32Array
+      | Float64Array
+      | string,
+    options?: sharp.SharpOptions,
+  ) => sharp.Sharp = sharp;
 }
